90f47a91a35abe669360d4b9823e172103519019
Yas2020 <yas.eftekhari@gmail.com>
2022-11-16 21:29:59 -0500

    2nd commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! 1st step og deploying ...'));
+app.get('/', (req, res) => res.send('Hello World! 2nd (SECOND) step og deploying ...'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
